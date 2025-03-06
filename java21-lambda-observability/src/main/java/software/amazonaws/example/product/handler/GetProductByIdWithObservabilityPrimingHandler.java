76963107ccd668e6151ee3fb7d6162df95cd88b8
Vadym Kazulkin <v.kazulkin@iplabs.de>
2025-03-06 20:08:05 +0100

    added snapstart and priming examples

--- java21-lambda-observability/src/main/java/software/amazonaws/example/product/handler/GetProductByIdWithObservabilityPrimingHandler.java
+++ java21-lambda-observability/src/main/java/software/amazonaws/example/product/handler/GetProductByIdWithObservabilityPrimingHandler.java
@@ -3,36 +3,52 @@
 
 package software.amazonaws.example.product.handler;
 
+import java.util.Map;
 import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.crac.Core;
 import org.crac.Resource;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent.ProxyRequestContext;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import software.amazon.awssdk.http.HttpStatusCode;
+import software.amazon.lambda.powertools.logging.CorrelationIdPaths;
+import software.amazon.lambda.powertools.logging.Logging;
+import software.amazon.lambda.powertools.tracing.CaptureMode;
+import software.amazon.lambda.powertools.tracing.Tracing;
+import software.amazon.lambda.powertools.tracing.TracingUtils;
 import software.amazonaws.example.product.dao.DynamoProductDao;
 import software.amazonaws.example.product.dao.ProductDao;
 import software.amazonaws.example.product.entity.Product;
 
-public class GetProductByIdWithPrimingHandler implements 
+public class GetProductByIdWithObservabilityPrimingHandler implements 
                  RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>, Resource {
 
 	private static final ProductDao productDao = new DynamoProductDao();
 	private final ObjectMapper objectMapper = new ObjectMapper();
+	private final static Logger logger = LogManager.getLogger(GetProductByIdWithObservabilityPrimingHandler.class);
+
 	
 	
-	public GetProductByIdWithPrimingHandler () {
+	public GetProductByIdWithObservabilityPrimingHandler () {
 		Core.getGlobalContext().register(this);
 	}
 	
 	@Override
 	public void beforeCheckpoint(org.crac.Context<? extends Resource> context) throws Exception {
-		productDao.getProduct("0");
+		APIGatewayProxyRequestEvent requestEvent = new APIGatewayProxyRequestEvent();
+		requestEvent.setPathParameters(Map.of("id","0"));
+		ProxyRequestContext requestContext= new ProxyRequestContext();
+	    requestContext.setRequestId("1234");
+		requestEvent.setRequestContext(requestContext);
+		this.handleRequest(requestEvent, new MockLambdaContext());
     }
 
 	@Override
@@ -41,17 +57,21 @@ public class GetProductByIdWithPrimingHandler implements
 	}
 
 	@Override
-	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
+	@Logging(logEvent = true, logResponse = true, samplingRate = 0.5, correlationIdPath = CorrelationIdPaths.API_GATEWAY_REST)
+    @Tracing(namespace ="ProductAPIWithPowerTools", captureMode = CaptureMode.RESPONSE_AND_ERROR)
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
 		String id = requestEvent.getPathParameters().get("id");
 		Optional<Product> optionalProduct = productDao.getProduct(id);
-
-		try {
+        try {
 			if (optionalProduct.isEmpty()) {
-				context.getLogger().log(" product with id " + id + "not found ");
+				logger.info(" product with id " + id + " not found ");
 				return new APIGatewayProxyResponseEvent().withStatusCode(HttpStatusCode.NOT_FOUND)
 						.withBody("Product with id = " + id + " not found");
 			}
-			context.getLogger().log(" product " + optionalProduct.get() + " not found ");
+			TracingUtils.putAnnotation("correlation_id", requestEvent.getRequestContext().getRequestId());
+
+            logger.debug("result of the product search: ");
+            logger.info(" product " + optionalProduct.get() + " found ");
 			return new APIGatewayProxyResponseEvent().withStatusCode(HttpStatusCode.OK)
 					.withBody(objectMapper.writeValueAsString(optionalProduct.get()));
 		} catch (Exception je) {
