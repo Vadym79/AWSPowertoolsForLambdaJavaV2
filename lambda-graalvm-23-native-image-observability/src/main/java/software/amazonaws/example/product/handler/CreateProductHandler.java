e65dc9bb0776d893a7c9935e4e6943c45dcbda1c
Vadym Kazulkin <v.kazulkin@iplabs.de>
2025-06-27 17:15:34 +0200

    updated to powertools for java v 2.1.1

--- lambda-graalvm-23-native-image-observability/src/main/java/software/amazonaws/example/product/handler/CreateProductHandler.java
+++ lambda-graalvm-23-native-image-observability/src/main/java/software/amazonaws/example/product/handler/CreateProductHandler.java
@@ -10,12 +10,12 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import software.amazon.awssdk.http.HttpStatusCode;
-import software.amazon.cloudwatchlogs.emf.logger.MetricsLogger;
-import software.amazon.cloudwatchlogs.emf.model.Unit;
 import software.amazon.lambda.powertools.logging.CorrelationIdPaths;
 import software.amazon.lambda.powertools.logging.Logging;
+import software.amazon.lambda.powertools.metrics.FlushMetrics;
 import software.amazon.lambda.powertools.metrics.Metrics;
-import software.amazon.lambda.powertools.metrics.MetricsUtils;
+import software.amazon.lambda.powertools.metrics.MetricsFactory;
+import software.amazon.lambda.powertools.metrics.model.MetricUnit;
 import software.amazon.lambda.powertools.tracing.CaptureMode;
 import software.amazon.lambda.powertools.tracing.Tracing;
 import software.amazonaws.example.product.dao.DynamoProductDao;
@@ -26,21 +26,20 @@ public class CreateProductHandler implements RequestHandler<APIGatewayProxyReque
 
 	private static final ProductDao productDao = new DynamoProductDao();
 	private static final ObjectMapper objectMapper = new ObjectMapper();
-	private final MetricsLogger metricsLogger = MetricsUtils.metricsLogger();
+	private static final Metrics metrics = MetricsFactory.getMetricsInstance();
 	
 	@Override
 	@Logging(logEvent = true, logResponse = true, samplingRate = 0.5, correlationIdPath = CorrelationIdPaths.API_GATEWAY_REST)
     @Tracing(namespace ="ProductAPINativeWithPowerTools", captureMode = CaptureMode.RESPONSE_AND_ERROR)
-    @Metrics(namespace = "ProductAPINativeWithPowerTools", service = "product-native", captureColdStart = true)
+    @FlushMetrics(namespace = "ProductAPINativeWithPowerTools", service = "product-native", captureColdStart = true)
 	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
 		try {
 			String requestBody = requestEvent.getBody();
 			Product product = objectMapper.readValue(requestBody, Product.class);
 			context.getLogger().log("create product: "+product);
 			productDao.putProduct(product);
-			metricsLogger.putMetric("SuccessfulProductCreation", 1, Unit.COUNT);
-            metricsLogger.putMetadata("correlation_id", requestEvent.getRequestContext().getRequestId());
-            context.getLogger().log("pushed metrics: ");
+			metrics.addMetric("SuccessfulProductCreation", 1, MetricUnit.COUNT);
+            metrics.addMetadata("correlation_id", requestEvent.getRequestContext().getRequestId());            context.getLogger().log("pushed metrics: ");
 			return new APIGatewayProxyResponseEvent().withStatusCode(HttpStatusCode.CREATED)
 					.withBody("Product with id = " + product.id() + " created");
 		} catch (Exception e) {
